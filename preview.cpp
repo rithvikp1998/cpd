5ce0aad64010f72486bab086a3908f55d182dbcc
Rithvik Patibandla <rithvikp98@gmail.com>
2017-05-29 14:46:41 +0530

    Added delete statements in preview.cpp

--- preview.cpp
+++ preview.cpp
@@ -11,17 +11,19 @@ QImage pdf_preview::requestImage(const QString &id, QSize *size, const QSize &re
         cout << "ERROR" << endl;
     }
 
-    Poppler::Page* pdfPage = document->page(0);
-    if (pdfPage == 0)
+    Poppler::Page* page = document->page(0);
+    if (page == 0)
     {
         cout << "ERROR" << endl;
     }
 
-    QImage image = pdfPage->renderToImage(72.0,72.0,0,0,pdfPage->pageSize().width(),pdfPage->pageSize().height());
+    QImage image = page->renderToImage(72.0,72.0,0,0,page->pageSize().width(),page->pageSize().height());
     if (image.isNull())
     {
         cout << "ERROR" << endl;
     }
 
     return image;
+    delete page;
+    delete document;
 }
