e7cc7a34cda91213b17b5fd3dabb1dcb0642c65d
Rithvik Patibandla <rithvikp98@gmail.com>
2017-06-05 12:26:37 +0530

    Moved from iostream to QMessageLogger

--- preview.cpp
+++ preview.cpp
@@ -1,23 +1,21 @@
 #include "preview.h"
 #include <poppler/qt5/poppler-qt5.h>
-#include <iostream>
-
-using namespace std;
+#include <QMessageLogger>
 
 /* The preview works by using poppler to convert a page of pdf to QImage */
 
 QImage QPdfPreview::requestImage(const QString &id, QSize *size, const QSize &requested_size) {
     Poppler::Document *document = Poppler::Document::load("/tmp/test.pdf");
     if (!document || document->isLocked())
-        cout << "ERROR" << endl;
+        qCritical("File '%s' does not exist or is locked!", qUtf8Printable("/tmp/test.pdf"));
 
     Poppler::Page *page = document->page(0);
     if (page == nullptr)
-        cout << "ERROR" << endl;
+        qCritical("File '%s' is empty?", qUtf8Printable("/tmp/test.pdf"));
 
     QImage image = page->renderToImage(72.0,72.0,0,0,page->pageSize().width(),page->pageSize().height());
     if (image.isNull())
-        cout << "ERROR" << endl;
+        qCritical("Error!");
 
     return image;
 }
