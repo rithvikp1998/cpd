d181abeafea433e021fd859e0fd20e12c5eefafd
Rithvik Patibandla <rithvikp98@gmail.com>
2017-07-22 11:11:13 +0530

    Small fix

--- backends/cups/src/print_frontend.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINT_FRONTEND_H
-#define PRINT_FRONTEND_H
-
-#include "frontend_helper.h"
-
-extern FrontendObj *f;
-int print_frontend_init(int argc, char **argv);
-
-#endif // PRINT_FRONTEND_H
